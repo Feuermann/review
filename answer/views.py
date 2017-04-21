e257fd144ec967060d0318ad2e99d731d692b0c7
feuermann <feuermann91@gmail.com>
2017-04-21 19:38:50 +0300

    Add oauth scope

--- answer/views.py
+++ answer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db.models import Prefetch
 from django.contrib.auth.models import User
 from rest_framework import viewsets, generics, permissions
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 from .models import Review, Comment
 from .serializers import ReviewSerializer, CommentSerializer, UserSerializer, SignUpSerializer
@@ -11,19 +12,19 @@ from answer.permisions import IsAuthenticatedOrCreate
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.prefetch_related('comment_set')
     serializer_class = ReviewSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
 
 
 class SignUp(generics.CreateAPIView):
